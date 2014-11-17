8c7aa84e0927c005ca011bda7017d781e64a0428
levink <levinkostya@yandex.ru>
2014-11-17 22:10:37 +0300

    Мигаем двумя диодами. Один не горит. Без delay().

--- led/led.ino
+++ led/led.ino
@@ -1,3 +1,5 @@
+unsigned long int last = 0;
+const long DELAY = 100;
 int led[3] = {2,3,4};
 int current = 0;
 
@@ -5,13 +7,18 @@ void setup() {
   pinMode(led[0], OUTPUT);
   pinMode(led[1], OUTPUT);
   pinMode(led[2], OUTPUT);
+  last = millis();
 }
+
 void loop() {
+  unsigned long int cur = millis();
+  if (cur - last <= DELAY) return;
+  
   for(int i=0; i < 3; i++)
   {
     if (i == current) analogWrite(led[i], 0);
     else analogWrite(led[i], 255);
   }
   current = (current + 1 ) %3;
-  delay(1000);
+  last = cur;
 }
